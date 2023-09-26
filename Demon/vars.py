34b1aabbf1de415d5d31d82b394f8b35d05dba3f
Sᴏᴜᴍᴏ <134998475+its-soumo@users.noreply.github.com>
2023-09-26 19:51:31 +0530

    Update vars.py

--- Demon/vars.py
+++ Demon/vars.py
@@ -1,11 +1,5 @@
 import os
-from os import environ
+from os import getenv
 
-def getConfig(name: str):
-    try:
-        return environ[name]
-    except:
-        return ""
-
-API_ID = environ("API_ID", "18990697")
-API_HASH = environ("API_HASH", "f4815b9a16cb03c2f5eabe8db1cb0903")
+API_ID = getenv("API_ID", "18990697")
+API_HASH = getenv("API_HASH", "f4815b9a16cb03c2f5eabe8db1cb0903")

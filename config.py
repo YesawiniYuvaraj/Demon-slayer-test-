0d47f2e1236268f9cc74f8134248056b858c47d9
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2023-09-26 19:37:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID =18990697
+API_HASH =f4815b9a16cb03c2f5eabe8db1cb0903
+BOT_TOKEN =6472612474:AAEtezGd2kv61vwh72ZGDVz31ALKq0G7JTE
+BOT_USERNAME =@Demon_slayer_test_bot
+MONGODB_URI =mongodb+srv://pbilakshan:AltruixUB@userbot.gdno4qt.mongodb.net/?retryWrites=true&w=majority
+db_name =Hashira
+collection_name =Default_1

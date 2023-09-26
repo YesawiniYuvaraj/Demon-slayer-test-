eb340bdb0bb3e65d3c222ba10fce52d9ebc171b4
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2023-09-26 19:44:49 +0530

    Update vars.py

--- Demon/vars.py
+++ Demon/vars.py
@@ -7,5 +7,5 @@ def getConfig(name: str):
     except:
         return ""
 
-API_ID = environ("API_ID", "")
-API_HASH = environ("API_HASH", "")
+API_ID = environ("API_ID", "18990697")
+API_HASH = environ("API_HASH", "f4815b9a16cb03c2f5eabe8db1cb0903")
